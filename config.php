e18e32504d2ae3c574aa3db36e46ec4f59616221
Webpurify Hosting <127546528+Wepurifyhosting@users.noreply.github.com>
2023-06-20 17:30:24 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['db']['host'] = 'sql.freedb.tech';
+$config['db']['port'] = '3306';
+$config['db']['username'] = 'freedb_gavyz';
+$config['db']['password'] = 'JvHC&mxxjs6mkDb';
+$config['db']['dbname'] = 'freedb_xen1231';
+
+$config['fullUnicode'] = true;
